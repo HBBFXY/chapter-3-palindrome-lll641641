2cde3439f9d510f9a959e53b3c831824580f778f
lll641641 <1624823256@qq.com>
2025-09-19 11:44:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ num_str = input()
 
 if len(num_str) != 5:
     print("错误")
-elif not (num_str[0] in '0123456789' and num_str[1] in '0123456789' and 
-          num_str[2] in '0123456789' and num_str[3] in '0123456789' and 
-          num_str[4] in '0123456789'):
+if not n.isdigit()
     print("错误")
 elif num_str[0] == num_str[4] and num_str[1] == num_str[3]:
     print("是回文数")
